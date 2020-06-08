ad528f3719032f18d45204aa2ded5e3c0b9dc1de
gyvareva <66568508+gyvareva@users.noreply.github.com>
2020-06-08 11:05:48 +0200

    Update game.js

--- game.js
+++ game.js
@@ -46,7 +46,7 @@ getNewQuestion = () => {
     if(availableQuestions.length==0 || questionCounter >= MAX_QUESTIONS){
         localStorage.setItem('mostRecentScore', score);
         // Go to the end page
-        return window.location.assign('/end.html');
+        return window.location.assign('./end.html');
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
