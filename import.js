63efa47c17988bd31b28bee68c8b64d9fc7fbea7
gyvareva <gyvareva@net-c.fr>
2020-06-12 13:22:47 +0200

    WIP - adding CSV to JSON conversion for questions.json

---
+++ import.js
@@ -0,0 +1,10 @@
+var csvinput = document.getElementById('csvinput').value;
+var jsonoutput = document.getElementById('jsonoutput').value;
+console.log(csvinput);
+console.log(jsonoutput);
+
+parse.csvinput;
+
+jsonoutput = csvtoJSON(csvinput);
+console.log(jsonoutput);
+//document.getElementById('csvinput').json2csv.toJSON = output;
