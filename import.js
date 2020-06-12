022451fde33eee98353af97568f2cf3774606ccc
RobotSpaceBear <47452069+RobotSpaceBear@users.noreply.github.com>
2020-06-12 23:52:04 +0200

    cleaned up some comments

--- import.js
+++ import.js
@@ -14,25 +14,10 @@ function gererConversionCsv()
     }
 
     composantSortie.value = csvtoJSON(composantEntree.value, separator);    
+    csvtoJSON(qzgrZ)
 }
 
 
 function initTest(){
     document.getElementById('csvinput').value = "question;choice1;choice2;choice3;choice4;answer\nQuestion 1;A1;A2;A3;A4;3\nQuestion 2;B1;B2;B3;B4;1";
 }
-
-
-
-/*
-récupérer le composant qui a le csv "l'entrée"
-tu prends la valeur du composant
-
-tu l'envoies dans le converter
-le converter le process et te ressort le json
-tu stocke le json
-
-tu prends le composant de sortie "jsonoutput"
-et tu mets la valeur du json dans le composant.value
-
-
-*/
