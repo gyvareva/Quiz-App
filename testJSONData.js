01d8c2a48f9424b508fe2be2065967817bd72f6a
gyvareva <gyvareva@net-c.fr>
2020-06-13 22:55:27 +0200

    Deleted unfiled testJSONData

--- testJSONData.js
+++
@@ -1,6 +0,0 @@
-function testeurJSON() {
-    
-    document.getElementById('csvinput').value = "question, choice1, choice2, choice3, choice4, answer\nQuelle est la PCO2 normale?,20,30,40,50\nQuelle est la couleur du cheval blanc d'henri 4?, bleu, rouge, blanc, jaune";
-
-}
-
